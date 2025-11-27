732267bab00f89c0afb1c8b477bcb409d389b07f
Abhiram <abhiramratheesh.399@gmail.com>
2025-11-27 22:36:44 +0530

    login update

--- js/login.js
+++ js/login.js
@@ -1,18 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { firebaseConfig } from "./firebase.js";  // your config file
 
-const auth = getAuth();
+// Initialize firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-function login() {
+window.login = function () {  // expose to HTML
   const email = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   signInWithEmailAndPassword(auth, email, password)
     .then(() => {
       localStorage.setItem("loggedIn", "true");
-      window.location.href = "admin.html";  
+      window.location.href = "admin.html";
     })
-    .catch(() => {
+    .catch((error) => {
       document.getElementById("error-msg").innerText = "Invalid Credentials!";
+      console.error(error);
     });
+
+    console.log("Login button clicked");
+
 }
